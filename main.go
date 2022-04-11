1dedeec44d46a71fb97612fdb94cd7f82f5ec85c
turnipsoup <jeremy@jeremyheckt.com>
2022-04-10 21:13:20 -0700

    can send html from embed static

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ package main
 //
 
 import (
-	_ "embed"
+	"embed"
 	"fmt"
 	"log"
 	"net/http"
@@ -16,17 +16,26 @@ import (
 //go:embed web/index.html
 var webIndex string
 
+//go:embed web
+var fileStrings embed.FS
+
 // Main
 
 func main() {
 	log.Print("Starting application")
 
+	// Getting the root of the application
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Print("Request recieved on /")
+		log.Print(r.UserAgent())
+
+		// Ensure that the browser knows it is HTML
+		w.Header().Set("Content-Type", "text/html")
 		fmt.Fprintln(w, webIndex)
 	})
 
-	// listen to port
+	// Listen on port
 	log.Print("Listening on port 5050")
 
 	http.ListenAndServe(":5050", nil)
